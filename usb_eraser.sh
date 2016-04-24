57a8175afb49cf39024a5d120031cf1f5eb17584
polmes <pol.developer1@gmail.com>
2016-04-24 23:43:07 +0200

    overhauled initial check, as it's already only run for such USB hardware

--- usb_eraser.sh
+++ usb_eraser.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
 # Known USB device info
-USB_UUID="06D4-6015"
 USB_LABEL="MESALLIS"
 USB_VENDOR="0781"
 USB_PRODUCT="5567"
 
 # Requirements
 	# usb_eraser.rules in /etc/udev/rules.d/ with the following content:
-	# ATTRS{idVendor}=="$USB_VENDOR", ATTRS{idProduct}=="$USB_PRODUCT", RUN+="sudo ~/git/RPi_USB_Eraser/usb_eraser.sh"
-
-# Known USB device info
-USB_UUID="06D4-6015"
-USB_LABEL="MESALLIS"
+	# ACTION=="add", ATTRS{idVendor}=="0781", ATTRS{idProduct}=="5567", RUN+="/home/pi/git/RPi_USB_Eraser/usb_eraser.sh"
 
 # Main
-function start_eraser {
-	sleep 10 # wait 10 seconds for last-moment regrets
-	# umount /dev/disk/by-label/MESALLIS # already unmounted
-	mkdosfs -F 32 -n "MESALLIS" -I $RETURN_UUID 2>> usb_eraser.log
-	echo "[$(date)] ... and we're done." >> usb_eraser.log
-}
-
-# Identify USB partition
-RETURN_UUID=$(readlink -f /dev/disk/by-uuid/$USB_UUID)
-RETURN_LABEL=$(readlink -f /dev/disk/by-label/$USB_LABEL)
-if [ $RETURN_UUID = $RETURN_LABEL ]; then
-	echo "[$(date)] USB_UUID and USB_LABEL match. Formatting in 10..." >> usb_eraser.log
-	start_eraser
-else
-	echo "[$(date)] USB_UUID and USB_LABEL did not match. Stopping now." >> usb_eraser.log
-fi
+echo "[$(date)] Formatting in 10..." >> /home/pi/git/RPi_USB_Eraser/usb_eraser.log
+sleep 10 # wait 10 seconds for last-moment regrets
+mkdosfs -F 32 -n "MESALLIS" -I $(readlink -f /dev/disk/by-label/$USB_LABEL) 2>> /home/pi/git/RPi_USB_Eraser/usb_eraser.log
+echo "[$(date)] ... and we're done." >> /home/pi/git/RPi_USB_Eraser/usb_eraser.log
