975c0c66d6f1def32bf825a395af1bb7e012e9f3
polmes <pol.developer1@gmail.com>
2016-04-24 20:33:08 +0200

    added mkdosfs command

--- usb_eraser.sh
+++ usb_eraser.sh
@@ -6,18 +6,18 @@ USB_LABEL="MESALLIS"
 # Main
 function start_eraser {
 	sleep 10 # wait 10 seconds for last-moment regrets
-	echo "Rock on" >> usb_eraser.log
+	# umount /dev/disk/by-label/MESALLIS # already unmounted
+	mkdosfs -F 32 -n "MESALLIS" -I $RETURN_UUID 2>> usb_eraser.log
+
+	echo "[$(date)] ... and we're done." >> usb_eraser.log
 }
 
 # Identify USB partition
 RETURN_UUID=$(readlink -f /dev/disk/by-uuid/$USB_UUID)
 RETURN_LABEL=$(readlink -f /dev/disk/by-label/$USB_LABEL)
 if [ $RETURN_UUID = $RETURN_LABEL ]; then
-	echo "$(date) - USB_UUID and USB_LABEL match. Formatting in 10..." >> usb_eraser.log
+	echo "[$(date)] USB_UUID and USB_LABEL match. Formatting in 10..." >> usb_eraser.log
 	start_eraser
 else
-	echo "$(date) - USB_UUID and USB_LABEL did not match. Stopping now." >> usb_eraser.log
+	echo "[$(date)] USB_UUID and USB_LABEL did not match. Stopping now." >> usb_eraser.log
 fi
-
-# umount /dev/disk/by-label/MESALLIS # already unmounted
-# mkfs -t xyz
