2617b4be0027ca9351392e3875f0baee3246b715
polmes <pol.developer1@gmail.com>
2016-04-24 20:54:30 +0200

    improved comments

--- usb_eraser.sh
+++ usb_eraser.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Known USB device info
+USB_UUID="06D4-6015"
+USB_LABEL="MESALLIS"
+USB_VENDOR="0781"
+USB_PRODUCT="5567"
+
+# Requirements
+	# usb_eraser.rules in /etc/udev/rules.d/ with the following content:
+	# ATTRS{idVendor}=="$USB_VENDOR", ATTRS{idProduct}=="$USB_PRODUCT", RUN+="sudo ~/git/RPi_USB_Eraser/usb_eraser.sh"
+
+# Known USB device info
 USB_UUID="06D4-6015"
 USB_LABEL="MESALLIS"
 
@@ -8,7 +19,6 @@ function start_eraser {
 	sleep 10 # wait 10 seconds for last-moment regrets
 	# umount /dev/disk/by-label/MESALLIS # already unmounted
 	mkdosfs -F 32 -n "MESALLIS" -I $RETURN_UUID 2>> usb_eraser.log
-
 	echo "[$(date)] ... and we're done." >> usb_eraser.log
 }
 
